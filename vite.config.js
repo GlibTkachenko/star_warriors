d56c9c9a93e6c4bd33fe8c8964dc8245c3577ef5
a1regg <gtkachenko7@gmail.com>
2025-11-13 18:03:58 +0000

    add favicon & mv sounds/ to public/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/star_warriors/', 
   root: './',
-  publicDir: 'sounds',
+  publicDir: 'public',
   server: {
     port: 3000,
     open: true
